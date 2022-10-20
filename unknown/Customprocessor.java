87a31a3ed2e34214d349c83109a093f20840b36c
root <root@karavan-7887689686-xdnpb>
2022-10-20 20:59:31 +0000

    2022-10-20

--- unknown/Customprocessor.java
+++ unknown/Customprocessor.java
@@ -1,8 +1,9 @@
+package org.msharma.camel
 import org.apache.camel.BindToRegistry;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 
-@BindToRegistry("Customprocessor")
+@BindToRegistry("mybean")
 public class Customprocessor implements Processor {
 
   public void process(Exchange exchange) throws Exception {
