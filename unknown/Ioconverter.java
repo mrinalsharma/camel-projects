e90afef6e1d1792c355b04c6c4ae20a4c0c16f19
root <root@karavan-578ccb5866-wlpbq>
2022-10-26 21:19:31 +0000

    2022-10-26

--- unknown/Ioconverter.java
+++ unknown/Ioconverter.java
@@ -4,13 +4,13 @@ import java.io.InputStream;
 import java.io.BufferedInputStream;
 import java.util.*;
 import org.apache.camel.Converter;
-
+import java.io.ByteArrayInputStream;
 @Converter(generateLoader = true)
 public class Ioconverter {
     @Converter
     public static InputStream toInputStream(java.util.LinkedList list) {
         StringBuilder str = new StringBuilder();
         list.forEach(value -> {str.append(value);});
-        return new BufferedInputStream(str.toString().getBytes());
+        return new ByteArrayInputStream(str.toString().getBytes());
     }
 }
