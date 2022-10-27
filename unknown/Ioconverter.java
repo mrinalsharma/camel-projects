265171ed859ccc7ac9fd5b2a4a6ba8d221e4f208
root <root@karavan-578ccb5866-wlpbq>
2022-10-27 15:47:49 +0000

    2022-10-27

--- unknown/Ioconverter.java
+++ unknown/Ioconverter.java
@@ -21,23 +21,24 @@ public class Ioconverter {
         return context.getTypeConverterRegistry().addTypeConverter(java.util.LinkedList.class, java.io.InputStream.class, new MyConverter());
     }
 
-    static class MyConverter implements TypeConverterSupport {
-            public <T> T convertTo(Class<T> type, Object value) {
+    static class MyConverter extends TypeConverterSupport {
+        @overide
+        public <T> T convertTo(Class<T> type, Object value) {
             // converter from value to the MyOrder bean
             return (T) new java.io.ByteArrayInputStream("My Hello World".getBytes());
         }
-
+        @overide
         public <T> T convertTo(Class<T> type, Exchange exchange, Object value) {
             // this method with the Exchange parameter will be preferd by Camel to invoke
             // this allows you to fetch information from the exchange during convertions
             // such as an encoding parameter or the likes
             return convertTo(type, value);
         }
-
+        @overide
         public <T> T mandatoryConvertTo(Class<T> type, Object value) {
             return convertTo(type, value);
         }
-
+        @overide
         public <T> T mandatoryConvertTo(Class<T> type, Exchange exchange, Object value) {
             return convertTo(type, value);
         }
