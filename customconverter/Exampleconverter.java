7d5b9c8f84db9b84dc5dc64f3c485021b8ef6c08
Mrinal Sharma <mrinalsharma@yahoo.com>
2022-11-02 16:28:47 -0400

    2022-11-02

--- customconverter/Exampleconverter.java
+++ customconverter/Exampleconverter.java
@@ -1,12 +1,19 @@
 import org.apache.camel.Converter;
 
-import java.io.*;
+import java.util.Arrays;
+import java.io.InputStream;
+import java.util.LinkedList;
+import java.io.ByteArrayInputStream;
 
 @Converter
 public class Exampleconverter {
      @Converter
-    public static InputStream toInputStream(File file) throws FileNotFoundException {
-        return new BufferedInputStream(new FileInputStream(file));
+    public static InputStream toInputStream(LinkedList elements) {
+		String [] stringArray = new String[elements.size()];
+		     for(int i =0; i < elements.size(); i++) {
+				stringArray[i] = (String) elements.get(i);
+			 }
+        return new ByteArrayInputStream(Arrays.toString(stringArray).getBytes());
     }
 
 }
